0197688f07f96918fa57441f66eb692cd56c02ea
Connor Gladwin <Cygladwin795@gmail.com>
2020-11-04 19:18:48 +0200

    Class name changes
    Changed class names for functions to work

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const output = document.querySelector('.output');
-const btn = document.querySelector('.btn');
+const rollBtn = document.querySelector('.rollBtn');
 let mod = document.getElementById('mod').value = 0;
 
 // adds the "selected class to the selected button"
@@ -8,13 +8,20 @@ for (let i = 0; i < diceButtons.length; i++) {
     diceButtons[i].addEventListener('click', function() {
         addClass(this, i);
     });
-}
+} 
 
 function addClass(button, i) {
     diceButtons.forEach(button => button.classList.remove('selected'));
     button.classList.add('selected');
 }
 
+function selectedDice(diceButtons) {
+    for (let i = 0; i < diceButtons.length; i++) {
+        if (diceButtons[i].classList.contains('selected')) {
+            console.log(diceButtons[i].value);
+        }
+    }
+}
 
 function handleRoll(max, min) {
     const rNum = Number.parseInt(Math.random() * (max - min) + min);
@@ -30,7 +37,7 @@ function handleRoll(max, min) {
     return (rNum + mod);
 }
 
-btn.addEventListener('click', () => {
-    let max = selectedDice();
-    output.textContent = handleRoll(max, 1);
+rollBtn.addEventListener('click', () => {
+    let max = selectedDice(diceButtons);
+    output.textContent = handleRoll(21, 1);
 });
